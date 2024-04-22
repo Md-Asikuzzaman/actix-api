2488d439a6306f1757269ade638ddfe85e2da7b1
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-22 15:24:15 +0600

    init rust app

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    let mut _name = "test";
+    let _age: i64 = 235675675656756756;
+    let _gpa = 3.23;
+
+
+    _name = "naim";
+
+    println!("Hello, world! {_name} {_age} {_gpa}");
+}
